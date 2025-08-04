e68a7683d95aee1f4c232f946826cc42571abf96
JB <jon@thebartletts.org>
2025-08-04 12:37:30 +0100

    Update const.py

--- custom_components/hildebrandglow_dcc/const.py
+++ custom_components/hildebrandglow_dcc/const.py
@@ -1,3 +1,13 @@
 """Constants for the Hildebrand Glow (DCC) integration."""
 
 DOMAIN = "hildebrandglow_dcc"
+
+# Virtual Entity Classifiers
+ELEC_CONSUMPTION_CLASSIFIER = "electricity.consumption"
+GAS_CONSUMPTION_CLASSIFIER = "gas.consumption"
+ELEC_COST_CLASSIFIER = "electricity.consumption.cost"
+GAS_COST_CLASSIFIER = "gas.consumption.cost"
+
+# Device Types
+ELECTRIC_METER = "electric_meter"
+GAS_METER = "gas_meter"
