8503344e552eb362002642992c11b92d27284702
JB <jon@thebartletts.org>
2025-09-11 16:47:57 +0100

    Update config_flow.py
    Implement setup options.

--- custom_components/hildebrandglow_dcc/config_flow.py
+++ custom_components/hildebrandglow_dcc/config_flow.py
@@ -10,7 +10,7 @@ import requests
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import CONF_DAILY_INTERVAL, CONF_TARIFF_INTERVAL, DOMAIN
@@ -21,10 +21,15 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required("username"): str,
         vol.Required("password"): str,
-        vol.Optional(CONF_DAILY_INTERVAL, default=15): vol.All(
+    }
+)
+
+OPTIONS_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_DAILY_INTERVAL): vol.All(
             vol.Coerce(int), vol.Range(min=1)
         ),
-        vol.Optional(CONF_TARIFF_INTERVAL, default=60): vol.All(
+        vol.Optional(CONF_TARIFF_INTERVAL): vol.All(
             vol.Coerce(int), vol.Range(min=1)
         ),
     }
@@ -53,14 +58,13 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Hildebrand Glow (DCC)."""
 
-    # Change version to force the setup to run and update stale setup/cached values.    
-    VERSION = 2
+    # Increment the version to force Home Assistant to reload the config flow.
+    VERSION = 6
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
-        # If left empty, simply show the form again
         if user_input is None:
             return self.async_show_form(
                 step_id="user", data_schema=STEP_USER_DATA_SCHEMA
@@ -68,7 +72,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         errors = {}
 
-        # Test authenticating with the API
         try:
             info = await validate_input(self.hass, user_input)
         except requests.Timeout as ex:
@@ -80,12 +83,47 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         except ValueError:
             _LOGGER.debug("Authentication Failed")
             errors["base"] = "invalid_auth"
-        except Exception as ex:  # pylint: disable=broad-except
+        except Exception as ex:
             _LOGGER.exception("Unexpected exception: %s", ex)
             errors["base"] = "unknown"
         else:
-            return self.async_create_entry(title=info["title"], data=user_input)
+            return self.async_create_entry(title=info["title"], data=user_input, options={CONF_DAILY_INTERVAL: 15, CONF_TARIFF_INTERVAL: 60})
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
+    
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: config_entries.ConfigEntry):
+        """Get the options flow for this handler. """
+
+        return OptionsFlowHandler()
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle an options flow for Hildebrand Glow (DCC)."""
+    
+    async def async_step_init(self, user_input=None):
+        """Handle the options flow."""
+        errors = {}
+
+        if user_input is not None:
+            daily_interval = user_input.get(CONF_DAILY_INTERVAL)
+            tariff_interval = user_input.get(CONF_TARIFF_INTERVAL)
+            
+            daily_interval = int(daily_interval) if daily_interval is not None else 1
+            tariff_interval = int(tariff_interval) if tariff_interval is not None else 1
+
+            if daily_interval < 5 or tariff_interval < 5:
+                errors["base"] = "Intervals of less than 5 minutes are not allowed to protect the Hildebrand Glow API from being overloaded."
+            
+            if not errors:
+                return self.async_create_entry(data=user_input)
+
+        data_schema = self.add_suggested_values_to_schema(
+            OPTIONS_SCHEMA,
+            self.config_entry.options,
+        )
+
+        return self.async_show_form(step_id="init", data_schema=data_schema, errors=errors)
