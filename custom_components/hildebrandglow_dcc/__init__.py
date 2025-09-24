a1d9ed713362623ad79c3039ca6be5a736c0556d
JB <jon@thebartletts.org>
2025-09-24 14:18:01 +0100

    Update __init__.py
    Additional debug code, to show the API error if its returned.

--- custom_components/hildebrandglow_dcc/__init__.py
+++ custom_components/hildebrandglow_dcc/__init__.py
@@ -29,6 +29,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         glowmarkt = await hass.async_add_executor_job(
             BrightClient, entry.data["username"], entry.data["password"]
         )
+    except requests.exceptions.HTTPError as ex:
+        _LOGGER.error("HTTP error during API authentication: Status Code %s - %s", ex.response.status_code, ex)
+        raise ConfigEntryNotReady(f"HTTP Error: {ex.response.status_code}") from ex
     except requests.Timeout as ex:
         _LOGGER.error("Timeout during API authentication: %s", ex)
         raise ConfigEntryNotReady(f"Timeout: {ex}") from ex
