d0d313456e5cb38a0d137c266d17721b487fbd23
JB <jon@thebartletts.org>
2026-05-15 11:16:22 +0100

    Add forgotten configuration entry for export classifiers - doh!
    Added configuration entry keys for daily and tariff intervals.

--- custom_components/hildebrandglow_dcc/const.py
+++ custom_components/hildebrandglow_dcc/const.py
@@ -2,16 +2,18 @@
 
 DOMAIN = "hildebrandglow_dcc"
 
-# Virtual Entity Classifiers
-ELEC_CONSUMPTION_CLASSIFIER = "electricity.consumption"
-GAS_CONSUMPTION_CLASSIFIER = "gas.consumption"
-ELEC_COST_CLASSIFIER = "electricity.consumption.cost"
-GAS_COST_CLASSIFIER = "gas.consumption.cost"
-
-# Device Types
-ELECTRIC_METER = "electric_meter"
-GAS_METER = "gas_meter"
-
-# ADD THESE LINES
+# Configuration Entry Keys
 CONF_DAILY_INTERVAL = "daily_interval"
 CONF_TARIFF_INTERVAL = "tariff_interval"
+
+# Resource Classifiers - Electricity
+ELEC_CONSUMPTION_CLASSIFIER = "electricity.consumption"
+ELEC_EXPORT_CLASSIFIER = "electricity.export"
+ELEC_IMPORT_REACTIVE_CLASSIFIER = "electricity.import.reactive"
+ELEC_EXPORT_REACTIVE_CLASSIFIER = "electricity.export.reactive"
+
+# Resource Classifiers - Gas
+GAS_CONSUMPTION_CLASSIFIER = "gas.consumption"
+
+# Note: Cost and Tariff resources are derived from these base classifiers 
+# in the sensor logic (e.g., electricity.consumption.cost)
